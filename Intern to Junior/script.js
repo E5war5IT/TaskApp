2e84ffb2cbd34891972970ffc170f68a9b2f3279
E5war5IT <91418370+E5war5IT@users.noreply.github.com>
2022-06-24 18:54:07 +0300

    Add files via upload

---
+++ Intern to Junior/script.js
@@ -0,0 +1,50 @@
+// JS Course
+
+// variable
+/*
+let variable = 20;
+variable  = 24;
+var name = 'age';
+const age = 21;
+console.log(age); // 21
+
+// type
+console.log(typeof 20); // number
+console.log(typeof 'welcome'); // string
+console.log(typeof true); // boolean
+// true false
+console.log(typeof null); // object {} []
+console.log(typeof undefined);
+console.log(typeof 10n); // bigint
+console.log(typeof Symbol(10)); // symbol
+*/
+
+// ПРЕОБРАЗОВАНИЕ ТИПОВ
+
+// СТРОКОВОЕ
+let value = true;
+alert(typeof value); // boolean
+value = String(value) // "true"
+alert(typeof value);
+
+// Численное
+alert("6" / "2") // 3
+
+let str = "124"; // string
+let num = Number(str);  // 124
+alert(typeof num); // number
+
+let age = Number("Любая строка вместо числа");
+alert(age); // NaN
+
+/*
+undefined NaN
+null 0
+true/false 1/0
+String  при ошибке NaN
+*/
+
+// Логическое
+Boolean(" ")
+
+// FUNCTION
